56a52e50b45d52cf75a3d0a3067cba746fec0eb1
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 22:30:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ list = ["Tim", "Jones","Bob Costas","Don Knotts"]
 def my_collect(array)
   i=0 
   name_collection = []
-  while i < array.lenght
+  while i < array.length
   name_collection.push yield(array[i])
 i+=1 
 
